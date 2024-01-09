da6100f622e053edee6b94e662380888f923cd94
aBrickThatCodes <brickcodes@outlook.com>
2024-01-09 11:38:58 +0100

    Basic subcommands

--- shop_cli/src/main.rs
+++ shop_cli/src/main.rs
@@ -1,13 +1,31 @@
 use shop_backend::*;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Client interface
+    Client,
+    /// Employee interface
+    Employee,
+}
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    Cli::parse();
     if let Err(e) = dotenvy::dotenv_override() {
         if !e.not_found() {
             bail!(e)
         }
     }
+    ShopBackend::init().await?;
     Ok(())
 }
